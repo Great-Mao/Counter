72bf7db3d14738345dced781be0d907982b48d55
Artem Trubitsin <artem-trubitsin@bk.ru>
2023-10-16 22:31:08 +0300

    Создал счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var titleButton: UIButton!
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textLabel.text = "0"
+        textLabel.textAlignment = .center
+        
     }
 
-
+    @IBAction func tapButton(_ sender: Any) {
+            counter += 1
+            textLabel.text = "Значение счётчика: \(counter)"
+        
+    }
+    
+    
 }
 
