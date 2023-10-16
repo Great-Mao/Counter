477ede641637064294fe6dfd7f702a408219a53f
Artem Trubitsin <artem-trubitsin@bk.ru>
2023-10-17 01:29:51 +0300

    Добавил закрытый уровень доступа для переменных и функций. Убрал лишние комментарии и поправил отступы. У кнопки заменил название с "title" на плюсик.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,25 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var textLabel: UILabel!
     
-    @IBOutlet weak var titleButton: UIButton!
-    var counter = 0
+    @IBOutlet weak private var textLabel: UILabel!
+    
+    @IBOutlet weak private var titleButton: UIButton!
+    private var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         textLabel.text = "0"
         textLabel.textAlignment = .center
-        
-    }
-
-    @IBAction func tapButton(_ sender: Any) {
-            counter += 1
-            textLabel.text = "Значение счётчика: \(counter)"
-        
+        titleButton.setTitle("+", for: .normal)
     }
     
-    
+    @IBAction private func tapButton(_ sender: Any) {
+        counter += 1
+        textLabel.text = "Значение счётчика: \(counter)"
+    }
 }
 
